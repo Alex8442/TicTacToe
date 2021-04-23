daee1fdd17fc94a970b54d9c5d7cba59928ac6de
Alex <78630640+Alex8442@users.noreply.github.com>
2021-04-23 21:54:09 +0200

    Fixed a bug, which allowed to overwrite players turns
    Additionally changed the code to english

--- main.c
+++ main.c
@@ -1,127 +1,140 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char spieler='o';
-char felder[10]={'0','1','2','3','4','5','6','7','8','9'};
-int runde;
+char player='o';
+char field[10]={'0','1','2','3','4','5','6','7','8','9'};
+int round;
 
 
 int main()
 {
     printf("_____________________________________________________\n");
     printf("|                                                   |\n");
-    printf("| Tic Tac Toe von Alex                              |\n");
-    printf("| Programmiersprache: C                             |\n");
+    printf("| Tic Tac Toe from Alex                             |\n");
+    printf("| Programming Language: C                           |\n");
     printf("|                                                   |\n");
-    printf("| Druecken sie eine Taste, um das Spiel zu starten. |\n");
+    printf("| Press a button to start.                          |\n");
     printf("|");
     printf("___________________________________________________|\n\n");
     getch();
-    anfang();
-
-    return 0;
+    start();
 }
 
-void anfang()
+void start()
 {
 
     system("CLS");
-    printf("Spieler O faengt an ");
+    printf("Player O starts ");
     getch();
-    spielfeld();
+    map();
 
 }
 
-void spielfeld()
+void map()
 {
     int i;
-    int auswahl;
-    int runde=1;
-    int gewonnen;
-    for(int i=0;i<10;i++) //schränkt die runden ein
+    int choice;
+    int round=1;
+    for(int i=0;i<10;i++) //schrÃ¤nkt die runden ein
     {
 
-    if (spieler=='O' && runde==2 || runde==4 || runde==6 || runde==8 || runde==10) { //wechselt den spieler von o zu x
-        spieler='X';
+    if (player=='O' && round==2 || round==4 || round==6 || round==8 || round==10) { //wechselt den spieler von o zu x
+        player='X';
     }
     else {
-    spieler='O'; // der spieler wird zu o
+    player='O'; // der spieler wird zu o
     }
 
-    //Rundenzähler//
+    //RundenzÃ¤hler//
     system("CLS");
-    if (runde<10) {
-    printf("Runde: %i\n",runde);
+    if (round<10) {
+    printf("Round: %i\n",round);
     }
     else {
-    printf("Runde: 9\n");
+    printf("Round: 9\n");
     }
 
-    //Spielfeld Darstellung//
-    printf(" %c | %c | %c \n",felder[1],felder[2],felder[3]);
+    //map Darstellung//
+    printf(" %c | %c | %c \n",field[1],field[2],field[3]);
     printf("-----------\n");
-    printf(" %c | %c | %c \n",felder[4],felder[5],felder[6]);
+    printf(" %c | %c | %c \n",field[4],field[5],field[6]);
     printf("-----------\n");
-    printf(" %c | %c | %c \n",felder[7],felder[8],felder[9]);
+    printf(" %c | %c | %c \n",field[7],field[8],field[9]);
     printf("\n\n");
-    printf("Spieler %c ist dran\n",spieler);
+    printf("Its the turn of %c\n",player);
 
-    printf("Wo wollen sie ihr Zeichen setzen? ");
+    printf("Where do you want to put your mark? ");
 
     //Setzen der Zeichen//
-    scanf("%i",&auswahl);
-    if (spieler=='x' || spieler=='X') {
-    felder[auswahl]='X';
+    scanf("%i",&choice);
+    if (player=='x' || player=='X') {
+        if (field[choice]=='X' || field[choice]=='O') {
+            printf("There is already a mark");
+            getch();
+            round--;
+        }
+        else {
+        field[choice]='X';
+        }
     }
 
-    if (spieler=='o' || spieler=='O') {
-    felder[auswahl]='O';
+    if (player=='o' || player=='O') {
+            if (field[choice]=='O' || field[choice]=='X') {
+                printf("There is already a mark");
+                getch();
+                round--;
+
+            }
+            else{
+                field[choice]='O';
+            }
+
     }
-    if (runde>9)
+    if (round>9)
     {
         system("CLS");
-        printf("Keiner hat gewonnen,Unentschieden");
+        printf("Nobody won, draw");
     }
 
-    //Bedingungen fürs Gewinnen, diese werden in jeder schleife geprüft.
-    if(felder[1]=='X' && felder[2]=='X' && felder[3]=='X' || felder[4]=='X' && felder[5]=='X' && felder[6]=='X' || felder[7]=='X' && felder[8]=='X' && felder[9]=='X') {
+    //Bedingungen fÃ¼rs Gewinnen, diese werden in jeder schleife geprÃ¼ft.
+    if(field[1]=='X' && field[2]=='X' && field[3]=='X' || field[4]=='X' && field[5]=='X' && field[6]=='X' || field[7]=='X' && field[8]=='X' && field[9]=='X') {
        system("CLS");
-       printf("Spieler X hat gewonnen");
+       printf("Player X won the game");
        exit(1);
    }
-   else if (felder[1]=='X' && felder[4]=='X' && felder[7]=='X' || felder[2]=='X' && felder[5]=='X' && felder[8]=='X' || felder[3]=='X' && felder[6]=='X' && felder[9]=='X')
+   else if (field[1]=='X' && field[4]=='X' && field[7]=='X' || field[2]=='X' && field[5]=='X' && field[8]=='X' || field[3]=='X' && field[6]=='X' && field[9]=='X')
    {
         system("CLS");
-        printf("Spieler X hat gewonnen");
+        printf("Player X won the game");
         exit(1);
    }
-   else if (felder[1]=='X' && felder[5]=='X' && felder[8]=='X' || felder[7]=='X' && felder[5]=='X' && felder[3]=='X')
+   else if (field[1]=='X' && field[5]=='X' && field[8]=='X' || field[7]=='X' && field[5]=='X' && field[3]=='X')
    {
         system("CLS");
-        printf("Spieler X hat gewonnen");
+        printf("Player X won the game");
         exit(1);
    }
 
 
-   if(felder[1]=='O' && felder[2]=='O' && felder[3]=='O' || felder[4]=='O' && felder[5]=='O' && felder[6]=='O' || felder[7]=='O' && felder[8]=='O' && felder[9]=='O') {
+   if(field[1]=='O' && field[2]=='O' && field[3]=='O' || field[4]=='O' && field[5]=='O' && field[6]=='O' || field[7]=='O' && field[8]=='O' && field[9]=='O') {
        system("CLS");
-       printf("Spieler O hat gewonnen");
+       printf("Player O won the game");
        exit(1);
    }
-   else if (felder[1]=='O' && felder[4]=='O' && felder[7]=='O' || felder[2]=='O' && felder[5]=='O' && felder[8]=='O' || felder[3]=='O' && felder[6]=='O' && felder[9]=='O')
+   else if (field[1]=='O' && field[4]=='O' && field[7]=='O' || field[2]=='O' && field[5]=='O' && field[8]=='O' || field[3]=='O' && field[6]=='O' && field[9]=='O')
    {
         system("CLS");
-        printf("Spieler O hat gewonnen");
+        printf("Player O won the game");
         exit(1);
 
    }
-   else if (felder[1]=='O' && felder[5]=='O' && felder[8]=='O' || felder[7]=='O' && felder[5]=='O' && felder[3]=='O')
+   else if (field[1]=='O' && field[5]=='O' && field[8]=='O' || field[7]=='O' && field[5]=='O' && field[3]=='O')
    {
         system("CLS");
-        printf("Spieler O hat gewonnen");
+        printf("Player O won the game");
         exit(1);
 
    }
-    runde++;
+    round++;
     }
 }
